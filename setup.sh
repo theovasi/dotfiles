8fcfc61ae611bf95117ad0d37b0512f50aeae28e
Theodoros Vasileiadis <theo.vasileiadis.work@gmail.com>
2017-07-29 18:53:01 +0300

    Fix YouCompleteMe installation error.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install software-properties-common
-sudo apt-get install python-software-properties
-sudo apt-get install i3 neovim git feh rofi scrot imagemagick i3lock dbus
-sudo apt-get install python-dev python-pip python3-dev python3-pip
-sudo apt-get install cmake make g++ xorg-dev libqt4-dev libcurl4-openssl-dev libavahi-compat-libdnssd-dev libssl-dev libx11-dev
-sudo apt-get install build-essential cmake
-sudo apt-get install python-dev python3-dev
+sudo apt-get install software-properties-common python-software-properties i3 neovim git feh rofi scrot imagemagick i3lock dbus python-dev python-pip python3-dev python3-pip
+sudo apt-get install cmake make g++ xorg-dev libqt4-dev libcurl4-openssl-dev libavahi-compat-libdnssd-dev libssl-dev libx11-dev build-essential cmake -y
 
 pip install setuptools
 pip install --upgrade neovim
@@ -18,7 +13,7 @@ pip3 install --upgrade neovim
 mkdir ~/.fonts ~/.scripts
 
 cp -rf i3 ~/.config/
-cp /fonts/* ~/.fonts
+cp fonts/* ~/.fonts
 cp .Xresources ~/ 
 cp -rf nvim/ ~/.config/
 cp scripts/i3lock.sh ~/.scripts
@@ -26,4 +21,5 @@ cp scripts/i3lock.sh ~/.scripts
 cd ~/.config/nvim/bundle/
 git clone https://github.com/Valloric/YouCompleteMe
 cd YouCompleteMe/
+git submodule update --init --recursive
 ./install.py
