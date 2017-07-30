053de01338743c6ccb9ea68065bda078bf19d0ef
Theodoros Vasileiadis <theo.vasileiadis.work@gmail.com>
2017-07-30 21:39:36 +0300

    Add oh-my-zsh.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update -y
 
-sudo apt-get install software-properties-common python-software-properties i3 i3blocks neovim git feh rofi scrot imagemagick i3lock dbus python-dev python-pip python3-dev python3-pip cmake make g++ xorg-dev libqt4-dev libcurl4-openssl-dev libavahi-compat-libdnssd-dev libssl-dev libx11-dev build-essential cmake -y
+sudo apt-get install software-properties-common python-software-properties i3 i3blocks neovim git feh rofi scrot imagemagick i3lock dbus python-dev python-pip python3-dev python3-pip cmake make g++ xorg-dev libqt4-dev libcurl4-openssl-dev libavahi-compat-libdnssd-dev libssl-dev libx11-dev build-essential cmake zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 pip install setuptools
 pip install --upgrade neovim
@@ -18,6 +19,7 @@ cp -rf nvim/ ~/.config/
 cp scripts/i3lock.sh ~/.scripts
 cp .bashrc ~/
 
+gconftool-2 --load gnome-terminal-conf.xml
 cd ~/.config/nvim/bundle/
 git clone https://github.com/Valloric/YouCompleteMe
 cd YouCompleteMe/
