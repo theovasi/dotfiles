ceeefc3b0b323f639e7c6e5a20f3af8e074fd00c
Theodoros Vasileiadis <theo.vasileiadis.work@gmail.com>
2018-08-02 09:47:23 +0400

    Add zshrc with base16 theme support.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-
 sudo apt-get update -y
-sudo apt-get install software-properties-common python-software-properties i3 i3blocks neovim git feh rofi scrot imagemagick i3lock dbus python-dev python-pip python3-dev python3-pip cmake make g++ xorg-dev libqt4-dev libcurl4-openssl-dev libavahi-compat-libdnssd-dev libssl-dev libx11-dev build-essential cmake zsh spotify-client compton synergy flake8 -y
+sudo apt-get install zsh i3 i3blocks neovim git feh rofi scrot imagemagick i3lock dbus python-dev python-pip python3-dev python3-pip cmake make g++ xorg-dev libqt4-dev libcurl4-openssl-dev libavahi-compat-libdnssd-dev libssl-dev libx11-dev build-essential cmake snap compton synergy flake8 -y
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -12,9 +8,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
-BASE16_SHELL=$HOME/.config/base16-shell/
-[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
-base16_ocean
 
 pip install setuptools autopep8
 pip install --upgrade neovim
@@ -22,7 +15,7 @@ pip3 install --upgrade neovim
 
 mkdir ~/.fonts ~/.scripts
 
-cp -rf i3 nvim compton.conf ~/.config/
+#cp -rf i3 nvim compton.conf ~/.config/
 cp fonts/* ~/.fonts
 cp scripts/i3lock.sh ~/.scripts
-cp .bashrc .Xresources ~/ 
+cp .zshrc .Xresources ~/ 
